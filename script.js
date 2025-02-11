b0388ea672c59c8e70423d70d0b504a7a319c589
Guillaume Ducharme-Vanier <e2151578@cmaisonneuve.qc.ca>
2025-02-11 14:02:12 -0500

    Ajout commentaire et ajout code

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ iconeMenuBg.addEventListener('click', () => {
     body.style.width = '';
   }
 });
+
+//Pour la vidéo
 const boutonVideo = document.querySelector('.button-video');
 const videoPetit = document.querySelector('.div-petit-video');
 const boutonPetitVideo = document.querySelector('.button-petit-video')
@@ -27,12 +29,13 @@ const body = document.body;
 boutonVideo.addEventListener('click', toggleVideo);
 boutonPetitVideo.addEventListener('click', toggleVideo);
 
+//Permet de mettre le mode actif sur la class div-petit-video et button-video, ce qui vient flouter
+//flouter le back-ground et mettre la petite vidéo en premier plan
 function toggleVideo() {
   const isActive = videoGrand.classList.toggle('active');
   videoPetit.classList.toggle('active'); 
   boutonVideo.classList.toggle('active');
 
-  // Empêche ou réactive le scroll
   if (isActive) {
     body.style.overflow = 'hidden';
   } else {
